3e97d817961963ee465e63af0b4f2eb61b5de472
Marynka <m.dovbnych@gmail.com>
2020-11-17 17:30:44 +0100

    create project

---
+++ src/main/java/com/imerish/project/PfSwChOProjectApplication.java
@@ -0,0 +1,13 @@
+package com.imerish.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfSwChOProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfSwChOProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imerish/project/PfSwChOProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imerish.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfSwChOProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
